3cb836c90d0108d4c2b9334662301a6428dd8f36
shanswami <shan.swami@gmail.com>
2014-09-16 18:57:50 -0700

    recording the file deletes

--- src/runningDancer.js
+++
@@ -1,7 +0,0 @@
-var RunningDancer = function(top, left, timeBetweenSteps, spanClass) {
-
-  Dancer.call(this, top, left, timeBetweenSteps, spanClass);
-}
-
-RunningDancer.prototype = Object.create(Dancer.prototype);
-RunningDancer.prototype.constructor = RunningDancer;
